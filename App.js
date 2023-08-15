0b88736714d4bed4ba24d693a360381ea096e816
Fernando <fernandocucci@gmail.com>
2023-08-15 14:58:26 +0900

    Navigation

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <CategoriesScreen></CategoriesScreen>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MealsCategories">
+        <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,17 @@
-import { Pressable, View, Text, StyleSheet } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
@@ -12,24 +19,32 @@ function CategoryGridTile({ title, color }) {
   );
 }
 
+export default CategoryGridTile;
+
 const styles = StyleSheet.create({
-  container: {
+  gridItem: {
     flex: 1,
     margin: 16,
     height: 150,
     borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
     shadowColor: "black",
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
-    backgroundColor: "white",
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
   },
   button: {
     flex: 1,
   },
-
+  buttonPressed: {
+    opacity: 0.5,
+  },
   innerContainer: {
     flex: 1,
     padding: 16,
+    borderRadius: 8,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -38,5 +53,3 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
-
-export default CategoryGridTile;
